d6da6797daed61e6daa59ac5c259a3ccce0fedc6
stogashi146 <stogashi146@gmail.com>
2023-12-10 13:29:55 +0900

    Eslintのインストール

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+const App = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,7 @@
+## プロジェクト作成
+
+プロジェクト作成コマンド
+`expo init Sanpo --template expo-template-blank-typescript`
+
+EsLint、Prettierの同銃
+https://deku.posstree.com/react-native/eslint-prettier-husky-lint-staged/
