fa58d37bfde1d1306b5c6bd7eb4478262c5e7b4f
stogashi146 <stogashi146@gmail.com>
2023-12-24 17:14:41 +0900

    タブバーを実装

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,22 +1,130 @@
-import { Stack } from 'expo-router'
+import { Text } from 'react-native'
+import { Tabs } from 'expo-router'
 import { theme } from '../theme'
+import { AntDesign, Octicons } from '@expo/vector-icons'
 
 const Layout = (): JSX.Element => {
   return (
-    <Stack
+    <Tabs
+      initialRouteName="theme/create"
       screenOptions={{
         headerStyle: {
           backgroundColor: theme.palette.background.main,
         },
         headerTintColor: theme.palette.text.main,
         headerTitle: 'Sanpo',
-        headerBackTitle: 'Back',
+        // headerBackTitle: 'Back',
         headerTitleStyle: {
           fontSize: 22,
           fontWeight: 'bold',
         },
       }}
-    />
+    >
+      <Tabs.Screen
+        name="community/posts"
+        options={{
+          href: 'community/posts',
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="home"
+              size={24}
+              color={
+                focused ? theme.palette.primary.main : theme.palette.gray.main
+              }
+            />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{
+                fontSize: theme.fontSize.sm,
+                color: focused
+                  ? theme.palette.primary.main
+                  : theme.palette.gray.main,
+              }}
+            >
+              みんなの投稿
+            </Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="theme/create"
+        options={{
+          title: 'お題',
+          href: 'theme/create',
+          tabBarIcon: ({ focused }) => (
+            <AntDesign
+              name="pluscircleo"
+              size={24}
+              color={
+                focused ? theme.palette.primary.main : theme.palette.gray.main
+              }
+            />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{
+                fontSize: theme.fontSize.sm,
+                color: focused
+                  ? theme.palette.primary.main
+                  : theme.palette.gray.main,
+              }}
+            >
+              お題
+            </Text>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="myPage/list"
+        options={{
+          href: 'myPage/list',
+          tabBarIcon: ({ focused }) => (
+            <Octicons
+              name="person"
+              size={24}
+              color={
+                focused ? theme.palette.primary.main : theme.palette.gray.main
+              }
+            />
+          ),
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{
+                fontSize: theme.fontSize.sm,
+                color: focused
+                  ? theme.palette.primary.main
+                  : theme.palette.gray.main,
+              }}
+            >
+              マイデータ
+            </Text>
+          ),
+        }}
+      />
+      {/* Null Screens */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'index',
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/log_in"
+        options={{
+          title: 'auth/log_in',
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="auth/sign_up"
+        options={{
+          title: 'auth/sign_up',
+          href: null,
+        }}
+      />
+    </Tabs>
   )
 }
 
---
+++ src/app/community/posts.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import CreateView from '../../components/Views/theme/CreateView'
+
+const posts: React.FC = () => {
+  return <CreateView />
+}
+export default posts
